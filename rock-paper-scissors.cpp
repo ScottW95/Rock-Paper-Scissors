7f4c2734c9314840417a4ad55a8e1c227faa2dcc
ScottW95 <91436863+ScottW95@users.noreply.github.com>
2023-02-06 02:28:10 +0000

    Finish code and debug program

--- rock-paper-scissors.cpp
+++ rock-paper-scissors.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 using namespace std;
 
-//Creates function for restarting game
-void restart_Game(){
-    if (restart == "Y" || restart == "y"){
-        continue;
-    } else {
-        cout << "Thanks for playing!";
-        break;
-    }
-
-}
-
 int main (){
 
 
@@ -19,29 +8,80 @@ int main (){
     srand(time(NULL));
     int computer = rand()%(3-1)+1;
 
-    //Creates variable for the player's choice
+    //Creates variable for the player's choice and score
     int player;    
-    int restart;
-
-    //Code for greeting the player
-
-    cout << "Hello! Would you like to play a game of Rock, Paper, Scissors? \n";
-    cout << "1.) Rock\n";
-    cout << "2.) Paper\n";
-    cout << "3.) Scissor\n";
-    cout << "Please enter the number of your selection: \n";
-
-    cin >> player;
-    cout << player << endl;
-    cout << computer << endl;
+    string restart;
+    int p_score = 0;
+    int c_score = 0;
 
     //Code for deciding the winner
+    do { 
 
-    if (player == 1 && computer == 1 ){
-        cout << "Sorry! Both you and the computer chose Rock.  It's a tie!\n";
-        cout << "Want to play again? Y/N";
-        cin
-    }
+
+        //Code for greeting the player
+        cout << "Hello! Welcome to my game of Rock, Paper, Scissors!\n";
+
+        cout << "1.) Rock\n";
+        cout << "2.) Paper\n";
+        cout << "3.) Scissor\n";
+        cout << "Please enter the number of your selection: \n";
+        cin >> player;
+
+
+            if (player == 1 && computer == 1 ){
+                cout << "Sorry! Both you and the computer chose Rock.  It's a tie!\n";
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N";
+                cin >> restart;
+            } else if (player == 1 && computer ==2){
+                cout << "Sorry! The computer chose paper and you lost!\n";
+                c_score = (c_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N \n";
+                cin >> restart;
+            } else if (player == 1 && computer == 3){
+                cout << "Congrats! The computer chose scissor and you won!\n";
+                p_score = (p_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N\n";
+                cin >> restart;
+            } else if (player == 2 && computer == 2 ){
+                cout << "Sorry! Both you and the computer chose Rock.  It's a tie!\n";
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N";
+                cin >> restart;
+            } else if (player == 2 && computer == 3){
+                cout << "Sorry! The computer chose scissor and you lost!\n";
+                c_score = (c_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N \n";
+                cin >> restart;
+            } else if (player == 2 && computer == 1){
+                cout << "Congrats! The computer chose rock and you won!\n";
+                p_score = (p_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N \n";
+                cin >> restart;
+            }  else if (player == 3 && computer == 3){
+                cout << "Sorry! Both you and the computer chose Rock.  It's a tie!\n";
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N";
+                cin >> restart;
+            } else if (player == 3 && computer == 1){
+                cout << "Sorry! The computer chose rock and you lost!\n";
+                c_score = (c_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N \n";
+                cin >> restart;
+            } else if (player == 3 && computer == 2){
+                cout << "Congrats! The computer chose paper and you won!\n";
+                p_score = (p_score + 1);
+                cout << "Your score is: " << p_score << ":" << c_score << endl;
+                cout << "Want to play again? Y/N \n";
+                cin >> restart;
+            } 
+
+    } while (restart != "n" || restart != "N");
 
 
 
